be038c6d33828cd072e79be622c5a854b57019a2
Thiago Moraes <thiago4x70@gmail.com>
2021-07-20 16:01:38 -0300

    implement a basic user crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://thiago:passw0rd@cluster0.vqq92.mongodb.net/test'
+    ),
+    UsersModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
